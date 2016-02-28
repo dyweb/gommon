2eca4ad302f467653958fd871751727f2a9f4147
Ce Gao <gaocegege@hotmail.com>
2016-02-28 11:21:11 +0800

    add the config support. there is some problems 1. mailgun/mailgun-go is too old

---
+++ types.go
@@ -0,0 +1,19 @@
+package common
+
+const (
+	// ConfigName defines the name of config file
+	ConfigName = ".ayi"
+	// AppName defines the name of app
+	AppName = "Ayi"
+
+	// Debug defines the Debug mode
+	Debug = "debug"
+	// Mailgun defines the Mailgun config
+	Mailgun = "mailgun"
+	//
+	MailgunDomain = "domain"
+	//
+	MailgunPrikey = "prikey"
+	//
+	MailgunPubkey = "pubkey"
+)
