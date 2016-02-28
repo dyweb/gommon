7ab8137ece028a349009efa3955af93c6acb12cb
Ce Gao <gaocegege@hotmail.com>
2016-02-28 15:39:50 +0800

    update: first version of the mail service

--- types.go
+++ types.go
@@ -10,10 +10,12 @@ const (
 	Debug = "debug"
 	// Mailgun defines the Mailgun config
 	Mailgun = "mailgun"
-	//
+	// MailgunDomain defines the domain name of the email server
 	MailgunDomain = "domain"
-	//
+	// MailgunPrikey defines the API key
 	MailgunPrikey = "prikey"
-	//
+	// MailgunPubkey now is useless, deprecated
 	MailgunPubkey = "pubkey"
+	// Sender defines the sender of the email
+	Sender = "sender"
 )
