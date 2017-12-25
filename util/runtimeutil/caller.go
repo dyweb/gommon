f75da1ace9fa1b9c5726845db73e30b1f8faee4c
at15 <at15@outlook.com>
2017-12-25 00:03:33 -0800

    [util][runtime] Add GetCallerFrame #32
    - copy from log2/identity.go which is copy from go-stack/stack.go
    - add unit test for value and pointer receiver, also util function for
    spliting package, struct and function

--- util/runtimeutil/caller.go
+++ util/runtimeutil/caller.go
@@ -18,6 +18,18 @@ func GetCallerPackage(skip int) string {
 	return fnName[:lastDot]
 }
 
+// see https://github.com/dyweb/gommon/issues/32
+// based on https://github.com/go-stack/stack/blob/master/stack.go#L29:51
+// TODO: not sure if calling two Next without checking the more value works for other go version
+func GetCallerFrame(skip int) runtime.Frame {
+	var pcs [3]uintptr
+	n := runtime.Callers(skip+1, pcs[:])
+	frames := runtime.CallersFrames(pcs[:n])
+	f, _ := frames.Next()
+	f, _ = frames.Next()
+	return f
+}
+
 // SplitPackageFunc returns package (w/o GOPATH) and function (w/ struct if presented) based on runtime.Frame.Function
 // Copied from runtime.Frame struct documentation
 // Func may be nil for non-Go code or fully inlined functions
@@ -39,4 +51,16 @@ func SplitPackageFunc(f string) (pkg string, function string) {
 		}
 	}
 	return f[:dot], f[dot+1:]
+}
+
+func SplitStructMethod(f string) (st string, function string) {
+	dot := strings.LastIndex(f, ".")
+	st, function = f[:dot], f[dot+1:]
+	if st[0] == '(' {
+		st = st[1:len(st)-1]
+	}
+	if st[0] == '*' {
+		st = st[1:]
+	}
+	return
 }
--- util/runtimeutil/calller_test.go
+++ util/runtimeutil/calller_test.go
@@ -1,9 +1,40 @@
 package runtimeutil
 
 import (
-	asst "github.com/stretchr/testify/assert"
 	"testing"
+	"runtime"
+
+	asst "github.com/stretchr/testify/assert"
 )
+
+type Foo struct {
+}
+
+func (f Foo) Bar() runtime.Frame {
+	return GetCallerFrame(0)
+}
+
+type FooPtr struct {
+}
+
+func (f *FooPtr) Bar() runtime.Frame {
+	return GetCallerFrame(0)
+}
+
+func TestGetCallerFrame(t *testing.T) {
+	t.Run("value receiver", func(t *testing.T) {
+		assert := asst.New(t)
+		foo := Foo{}
+		fm := foo.Bar()
+		assert.Equal("github.com/dyweb/gommon/util/runtimeutil.Foo.Bar", fm.Function)
+	})
+	t.Run("pointer receiver", func(t *testing.T) {
+		assert := asst.New(t)
+		foo := FooPtr{}
+		fm := foo.Bar()
+		assert.Equal("github.com/dyweb/gommon/util/runtimeutil.(*FooPtr).Bar", fm.Function)
+	})
+}
 
 func TestSplitPackageFunc(t *testing.T) {
 	assert := asst.New(t)
@@ -21,5 +52,24 @@ func TestSplitPackageFunc(t *testing.T) {
 		p, f := SplitPackageFunc(c.Function)
 		assert.Equal(c.Package, p)
 		assert.Equal(c.RealFunction, f)
+	}
+}
+
+func TestSplitStructMethod(t *testing.T) {
+	assert := asst.New(t)
+
+	cases := []struct {
+		Function string
+		Struct   string
+		Method   string
+	}{
+		{"Foo.Bar", "Foo", "Bar"},
+		{"(*FooPtr).Bar", "FooPtr", "Bar"},
+	}
+
+	for _, c := range cases {
+		st, f := SplitStructMethod(c.Function)
+		assert.Equal(c.Struct, st)
+		assert.Equal(c.Method, f)
 	}
 }
