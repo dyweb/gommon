7d9a3b2071ef93df42f3b0a128f2d575083ba414
at15 <at15@outlook.com>
2018-01-08 15:36:11 -0800

    [go][test] Fix #36 empty struct is reused in go1.8
    - in go1.7 and go1.8, empty struct is just one same value, thus the
    pointer to empty struct are same, in go1.9 and tip this is not the
    case, I am using go1.9 when testing out this in playground thus not
    aware of that, the solution is to add field with different value in the
    test and it pass 1.7, 1.8, 1.9, latest
    - there is already docker based test, in scripts/docker-compose.yml and
    .ayi.yml, `Ayi run docker-test` should run them all

--- playground/stdlib/stdlib_ptr_test.go
+++ playground/stdlib/stdlib_ptr_test.go
@@ -7,15 +7,22 @@ import (
 )
 
 type ptrFoo struct {
+	// NOTE: this attribute is needed, because otherwise in go1.7 and 1.8 emtpy struct seems to be optimized by compiler
+	// to use same value, and the pointer to and empty struct would be same
+	// p1 := &ptrFoo{}
+	// p2 := &ptrFoo{}
+	// p1 == p2 // true in 1.7 and 1.8, false in 1.9 and tip
+	// https://github.com/dyweb/gommon/issues/36
+	foo string
 }
 
 func TestPtrCompare(t *testing.T) {
 	assert := asst.New(t)
 
-	o := ptrFoo{}
+	o := ptrFoo{foo: "o"}
 	p1 := &o
 	p2 := &o
-	p3 := &ptrFoo{}
+	p3 := &ptrFoo{foo: "p3"}
 	assert.True(p1 == p2)
 	assert.False(p1 == p3)
 	assert.False(p2 == p3)
