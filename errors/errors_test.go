0674b0eb806bdc5dabdac8e8c4b8fa9835eec682
at15 <at15@outlook.com>
2018-03-05 20:24:48 -0800

    [error] Implement fmt.Formatter #62
    - didn't support print stack ...

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -3,14 +3,17 @@ package errors
 import (
 	"os"
 	"testing"
+	"fmt"
 
 	asst "github.com/stretchr/testify/assert"
+	"github.com/dyweb/gommon/util/testutil"
 )
 
 func TestNew(t *testing.T) {
 	assert := asst.New(t)
 	err := New("don't let me go")
 	assert.NotNil(err)
+	assert.Equal("don't let me go", fmt.Sprintf("%v", err))
 	terr, ok := err.(TracedError)
 	assert.True(ok)
 	printFrames(terr.ErrorStack().Frames())
@@ -32,20 +35,27 @@ func TestWrap(t *testing.T) {
 	assert.Nil(n)
 
 	errw := Wrap(os.ErrClosed, "can't open closed file")
+	assert.Equal("can't open closed file: file already closed", fmt.Sprintf("%v", errw))
 	terr, ok := errw.(TracedError)
 	assert.True(ok)
-	printFrames(terr.ErrorStack().Frames())
+	if testutil.Dump().B() {
+		printFrames(terr.ErrorStack().Frames())
+	}
 	assert.Equal(3, len(terr.ErrorStack().Frames()))
 
 	errw = Wrap(freshErr(), "wrap again")
 	terr, ok = errw.(TracedError)
 	assert.True(ok)
-	printFrames(terr.ErrorStack().Frames())
+	if testutil.Dump().B() {
+		printFrames(terr.ErrorStack().Frames())
+	}
 	assert.Equal(4, len(terr.ErrorStack().Frames()))
 
 	errw = Wrap(wrappedStdErr(), "wrap again")
 	terr, ok = errw.(TracedError)
 	assert.True(ok)
-	printFrames(terr.ErrorStack().Frames())
+	if testutil.Dump().B() {
+		printFrames(terr.ErrorStack().Frames())
+	}
 	assert.Equal(4, len(terr.ErrorStack().Frames()))
 }
