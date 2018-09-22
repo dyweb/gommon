2205cab9d33ed81f1580e1d6ec8ec4a7da9a2b4f
at15 <at15@outlook.com>
2018-09-21 21:13:35 -0700

    [httputil] Add test for unix domain sock server

--- util/httputil/unix_test.go
+++ util/httputil/unix_test.go
@@ -1,12 +1,18 @@
 package httputil_test
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
+	requir "github.com/stretchr/testify/require"
+
+	"github.com/dyweb/gommon/util/fsutil"
 	dhttputil "github.com/dyweb/gommon/util/httputil"
 	"github.com/dyweb/gommon/util/testutil"
 )
@@ -41,4 +47,34 @@ func TestNewPooledUnixTransport(t *testing.T) {
 		t.Log(string(testutil.GetBody(t, c, srv.URL+"/docker/proxy/version")))
 		t.Log(string(testutil.GetBody(t, c, srv.URL+"/docker/proxy/info")))
 	})
+}
+
+func TestListenAndServeUnix(t *testing.T) {
+	require := requir.New(t)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+	srv := http.Server{
+		Handler: mux,
+	}
+	if fsutil.FileExists("/tmp/gommon.sock") {
+		require.Nil(os.Remove("/tmp/gommon.sock"))
+	}
+	go func() {
+		// curl --unix-socket /tmp/gommon.sock http://abc/ping
+		err := dhttputil.ListenAndServeUnix(&srv, "/tmp/gommon.sock")
+		if err != nil {
+			t.Logf("error start server: %s", err)
+		}
+		require.Nil(err)
+	}()
+	time.Sleep(1 * time.Millisecond)
+	tr := dhttputil.NewPooledUnixTransport("/tmp/gommon.sock")
+	c := &http.Client{Transport: tr}
+	s := string(testutil.GetBody(t, c, "http://api/ping"))
+	require.Equal("pong", s)
+	srv.Shutdown(context.Background())
+	os.Remove("/tmp/gommon.sock")
 }
